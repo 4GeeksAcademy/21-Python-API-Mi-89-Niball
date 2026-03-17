734ce8f1b5742736562761c7c386cec5df6824fa
Naboulibear <108967590+Naboulibear@users.noreply.github.com>
2026-03-17 14:07:01 +0000

    finished

---
+++ src/app.py
@@ -0,0 +1,38 @@
+
+from flask import Flask,jsonify, request
+app = Flask(__name__)
+
+todos = [
+    { "label": "My first task", "done": False },
+    { "label": "My second task", "done": False }
+]
+
+@app.route('/todos', methods=['GET'])
+def hello_world():
+    json_text = jsonify(todos)
+    return json_text 
+
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    request_body = request.json
+    print("Incoming request with the following body", request_body)
+
+    todos.append(request_body)   # 🔥 THIS is what you were missing
+
+    return jsonify(todos)        # 🔥 return updated list
+
+
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    print("This is the position to delete:", position)
+    todos.pop(position)
+    return jsonify(todos)
+
+
+@app.route('/tests', methods=['GET'])
+def good_morning():
+    return "Hello Nibal!!!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3245, debug=True)
+
